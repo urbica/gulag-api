d3b7882b43c470d1f21c6c6b6738c73c3ef2e695
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-02-23 14:19:33 +0300

    fix: new ctx

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const Koa = require('koa')
+const Koa = require('koa');
 
-const app = new Koa()
+const app = new Koa();
 
 app.use(async (ctx) => {
-  ctx.body = 'Hello World'
-})
+  ctx.body = 'Hello World!';
+});
 
-module.exports = app
+module.exports = app;
