356a34d51c2e20794af78a4a1e6addce79e63d04
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-03-01 13:41:50 +0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 require('dotenv').config();
 
-app.listen(3000, () => {
-  console.log('Server started ad localhost:3000');
+app.listen(3001, () => {
+  console.log('Server started ad localhost:3001');
 });
--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('kcors');
 const router = require('./router');
 
 const app = new Koa();
 
-app.use(bodyParser());
-
-app.use(router.routes()).use(router.allowedMethods());
+app
+  .use(bodyParser())
+  .use(cors())
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 module.exports = app;
