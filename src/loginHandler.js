8cc8cd8312c4b294c59195485d41be1f1beb02c6
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-05-10 13:22:23 +0300

    fix: login handler hot fix

--- src/loginHandler.js
+++ src/loginHandler.js
@@ -1,5 +1,4 @@
 const Router = require('koa-router');
-const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 
 const router = new Router();
@@ -7,12 +6,7 @@ const router = new Router();
 router.post('/', async ctx => {
   const { email, password } = ctx.request.body;
 
-  const hash = crypto
-    .createHash('sha256')
-    .update(password)
-    .digest('base64');
-
-  if (hash !== process.env.API_PASSWORD) {
+  if (password !== process.env.API_PASSWORD) {
     ctx.throw(401, 'Invalid credentials');
   }
 
